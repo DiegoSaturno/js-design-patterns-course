d74eb7f13f878085e9eec538e36b3494f4e475a8
Diego Oliveira <diegosetubal10@hotmail.com>
2019-04-16 15:42:02 -0300

    Cat clicker v2

--- cat-clicker/js/main.js
+++ cat-clicker/js/main.js
@@ -1,12 +1,35 @@
-var counterRef = document.getElementById('counter');
-
-var catButton = document.getElementById('cat');
-catButton.addEventListener('click', function() {
-    somarValorContador();
+window.addEventListener('load', function() {
+    setCatName(cat);
+    setCatName(challengerCat);
 });
 
-function somarValorContador() {
-    var value = parseInt(counterRef.innerText);
-    
-    counterRef.innerText = value + 1;
+var cat = {
+    name: 'Cat',
+    catNameDom: document.getElementById('cat-name'),
+    domRef: document.getElementById('cat'),
+    counterRef: document.getElementById('cat-counter')
+}
+
+var challengerCat = {
+    name: 'Challenger Cat',
+    catNameDom: document.getElementById('challenger-cat-name'),
+    domRef: document.getElementById('challenger-cat'),
+    counterRef: document.getElementById('challenger-cat-counter')
+}
+
+cat.domRef.addEventListener('click', function() {
+    somarValorContador(cat.counterRef);
+});
+
+challengerCat.domRef.addEventListener('click', function() {
+    somarValorContador(challengerCat.counterRef);
+})
+
+function somarValorContador(ref) {
+    var value = parseInt(ref.innerText);    
+    ref.innerText = value + 1;
+}
+
+function setCatName(cat) {
+    cat.catNameDom.innerText = cat.name;
 }
